75cada9a4f7b833bf3a53e227f496c3fc432cccb
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-22 13:14:30 -0300

    Refactor user update route to use PUT method

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,23 +5,27 @@ const UsersModel = require('../models/users');
 
 const router = express.Router()
 
-router.post("/cadastrar", async (req, res) => {
+router.put('/:id', async (req, res) => {  
   // #swagger.tags = ['Usuários']
-  // #swagger.summary = 'Criar um usuário'
+  // #swagger.summary = 'Alterar uma usuário'
 
   try {
-    const user = await UsersModel.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(422).json({ error: 'Usuário já existe.' });
-    }
+    const id = req.params.id;
 
     req.body.password = await bcrypt.hash(req.body.password, 10);
 
-    const newUser = await UsersModel.create(req.body);
-    res.json(newUser);
+    const updatedData = req.body;
+    const options = {
+      new: true,
+    };
 
+    await UsersModel.findByIdAndUpdate(
+      id, updatedData, options
+    );
+
+    res.status(200).json({ success: true });
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(400).json({ error: error });
   }
 });
 
